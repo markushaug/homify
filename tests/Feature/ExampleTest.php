9700c0b6e31c5eb46860539a1cd9a868088198c1
Markus Haug <mh@haugmarkus.de>
2018-02-05 11:20:47 +0100

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,12 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ExampleTest extends TestCase
 {
+    
+    use WithoutMiddleware;
     /**
      * A basic test example.
      *
