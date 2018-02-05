f2fd39d87107a7a60e441f724c90b493760a9d7e
Markus Haug <mh@haugmarkus.de>
2018-02-05 11:16:48 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use WithoutMiddleware;
+    
 }
