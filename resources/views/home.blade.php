ba24bffdf92c282ba016fbb519f1e2a59d00fa67
Markus Haug <markushaug99@icloud.com>
2018-12-24 16:04:26 +0100

    Fronted State changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,16 @@
 
     <ul id="ListArea">
         @foreach ($things as $thing)
-        <li style="cursor: pointer;" onclick="toggle({{ $thing }})" id="{{ $thing->id }}" data-status="@php if($thing->state == 'ON') { echo 0; } else { echo 1; } @endphp">
+        <li style="cursor: pointer;" onclick="toggle({{ $thing }})" id="{{ $thing->id }}" data-status="@php if($thing->state == '1') { echo 0; } else { echo 1; } @endphp">
             <div class="Text">
                 <h3>{{ $thing->thing }}</h3>
             </div>
             <div class="Image">
-                <img @switch($thing->thingType) @case('Switcher') @php if($thing->state == 'ON') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"';
+                <img @switch($thing->thingType) @case('Switcher') @php if($thing->state == '1') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"';
                 } else { echo 'src="/images/ThingTypes/'.$thing->thingType.'_OFF.png"'; } @endphp @break @case('Speaker')
-                @php if($thing->state == 'ON') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"'; } else { echo
+                @php if($thing->state == '1') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"'; } else { echo
                 'src="/images/ThingTypes/'.$thing->thingType.'_OFF.png"'; } @endphp @break @case('Light') @php if($thing->state
-                == 'ON') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"'; } else { echo 'src="/images/ThingTypes/'.$thing->thingType.'_OFF.png"';
+                == '1') { echo 'src="/images/ThingTypes/'.$thing->thingType.'_ON.png"'; } else { echo 'src="/images/ThingTypes/'.$thing->thingType.'_OFF.png"';
                 } @endphp @break @default Default case... @endswitch>
             </div>
             <div class="Bar">
